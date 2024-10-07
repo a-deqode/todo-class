04a1fbf7622d8d322c8c282548f192740c769e72
This is second repository <achouksey@deqode.com>
2024-10-07 18:49:18 +0530

    add new functionality

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,17 +1,22 @@
-import React from 'react'; 
-const AddTodo = ({ input, addTodo, handleInputChange }) => { 
-  return (
-    <form onSubmit={addTodo}> // Set the form submission handler to addTodo
-      <input
-        type="text"
-        value={input} 
-        onChange={handleInputChange}
-        placeholder="Add a new todo" 
-        required 
-      />
-      <button type="submit">Add</button> // Button to submit the form
-    </form>
-  );
-};
+import React, { Component } from 'react'; 
+
+class AddTodo extends Component { 
+  render() {
+    const { input, addTodo, handleInputChange } = this.props;
+
+    return (
+      <form onSubmit={addTodo}> 
+        <input
+          type="text" 
+          value={input}
+          onChange={handleInputChange} 
+          placeholder="Add a new todo" 
+          required 
+        />
+        <button type="submit">Add</button> 
+      </form>
+    );
+  }
+}
 
 export default AddTodo; 
